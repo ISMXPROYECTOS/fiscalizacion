dec7abd73409699945ee0c2a68e1d3ce93795517
Emmanuel Ruiz <emmanuelre93@gmail.com>
2019-05-20 22:07:51 -0500

    Modelos y controladores

---
+++ app/Http/Controllers/municipiocontroller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class municipiocontroller extends Controller
+{
+    //
+}
---
+++ app/bitacoradeestatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class bitacoradeestatus extends Model
+{
+    protected $table = 'bitacoradeestatus';
+    
+}
