754b87d8d3f51a823db93f525406e4ad22ba3a5e
Juan Amaya <amayajuan95@gmail.com>
2019-07-05 14:04:51 -0500

    Ejemplo Basico de Gafete con QR

--- app/Http/Controllers/GafetesController.php
+++ app/Http/Controllers/GafetesController.php
@@ -6,18 +6,35 @@ use App\Gafetes;
 use App\Inspector;
 use App\EjercicioFiscal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 
 class GafetesController extends Controller
 {
 
 	public function create(Request $request){
 
+		// Este metodo debe crear un gafete de inspector que incluya ejercicio fiscal, inspector, folio, created_at, fecha vence, qr, estatus
+
+		// el gafete a mostrar debe tener: Nombre, Apellido, Puesto, Vigencia, Foto
+
+		// el codigo qr debe mandarnos a una vista rellenada por los datos del inspector
+
+		// primero recibimos el inspector al que queremos generar su gafete
+
+
+		
+
 		$id = $request->input('id');
 		$inspector = Inspector::find($id);
 
-		var_dump($inspector);
-		die();
-		return \QrCode::size(300)->generate("www.crealab.com.mx");
+		// año fiscal
+		$ejercicio_fiscal = EjercicioFiscal::where('anio', date("Y"))->get();
+
+		//var_dump(url('/perfil/inspector/'.$inspector->id));
+		//die();
+		
+		
+		return \QrCode::size(300)->generate(url('/perfil/inspector/'.$inspector->id));
 	}
     
 }
