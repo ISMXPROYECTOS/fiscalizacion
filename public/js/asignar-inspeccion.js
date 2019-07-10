529378f21083ec67c666824f3cfa528e00ecfaa4
Emmanuel Ruiz Estrada <emmanuelre93@gmail.com>
2019-07-10 11:06:45 -0500

    Revert "Ya casi queda el mostrar folios por inspector"
    This reverts commit ed08ca8e76bd211de3b40e06e436417a8ffffd1a.

--- public/js/asignar-inspeccion.js
+++ public/js/asignar-inspeccion.js
@@ -26,9 +26,7 @@ $(document).ready(function(){
 
 
     $(".form-check-input").each(function(){
-
         $('#inspector-'+$(this).val()).click(function(){
-
         	var data = {
 				'tipoinspeccion' :$('#tipoinspeccion-asignar').val(),
 				'cantidad' : $('#cantidad-asignar').val(),
@@ -36,18 +34,13 @@ $(document).ready(function(){
 				'inspectores-asignar': JSON.stringify($('[name="inspectores-asignar[]"]').serializeArray())
 			}
 
-			console.log(data);
-
 			$.ajax({
 	            url: url + '/inspecciones/obtener-folios-inspecciones',
 	            data: data,
 	            type: 'post',
             	headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
 	            success: function (response) {
-
-	            	console.log('#folio-inicio-'+$(this).val())
-	                //$('#folio-inicio-'+$(this).val()).text(response.folioinicio);
-	                //$('#folio-fin-'+$(this).val()).text(response.foliofin);
+	                $('#cantidadexistente-asignar').val(response);
 	            } 
 	        });
 		});
