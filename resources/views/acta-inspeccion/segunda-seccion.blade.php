7242abf5aa0b02413cc5ba773e42484aecd68cec
ANDYRAT <andyrat_96@hotmail.com>
2019-12-16 11:06:26 -0500

    omitir
    Se intenta agilizar la descarga de inspecciones

---
+++ resources/views/acta-inspeccion/segunda-seccion.blade.php
@@ -0,0 +1,39 @@
+<p class="aic-contenido-general mb-0">Finalmente el suscrito, le informa al visitado el derecho que tiene a formular observaciones en el acto de la diligencia y ofrecer pruebas en relación a los hechos contenidos en ella, o bien, por escrito, hacer uso de tal derecho dentro del término de 10 días hábiles siguientes a la fecha en que se hubiere levantado la presente diligencia, quien manifiesta que ______________________________________________________________________________________________________________</p>
+<p class="aic-contenido-general mbt-0">________________________________________________________________________________________________________________________________________</p>
+<p class="aic-contenido-general mbt-0">________________________________________________________________________________________________________________________________________</p>
+<p class="aic-contenido-general mbt-0">________________________________________________________________________________________________________________________________________</p>
+<p class="aic-contenido-general mbt-0">________________________________________________________________________________________________________________________________________</p>
+
+<p class="aic-contenido-general mb-0">Independientemente de lo anterior, el verificador formula las siguientes observaciones: __________________________________________________________________</p>
+<p class="aic-contenido-general mbt-0">________________________________________________________________________________________________________________________________________</p>
+<p class="aic-contenido-general mbt-0">________________________________________________________________________________________________________________________________________</p>
+<p class="aic-contenido-general mbt-0">________________________________________________________________________________________________________________________________________</p>
+<p class="aic-contenido-general mbt-0">________________________________________________________________________________________________________________________________________</p>
+<p class="aic-contenido-general">Asimismo, derivado  de los resultados de la presente Inspección, con fundamento en el artículo 51, fracción I, del Código Fiscal Municipal del Estado de Quintana Roo, en este acto se le requiere al  contribuyente o a su representante legal, para que dentro del término de diez días contados a  partir del día siguiente al que se realiza esta diligencia, comparezca en las oficinas de la Dirección de Fiscalización dependiente de la Tesorería Municipal de Benito Juárez, Quintana Roo, ubicada en Edificio anexo al Palacio Municipal  en la Avenida Nader sin número, Supermanzana 5, de esta ciudad de Cancún, Quintana Roo, para exhibir la documentación que no exhibe en la presente diligencia, apercibido que en caso de no hacerlo se procederá de conformidad con lo establecido en el artículo 509, fracciones IV y VII del Bando de Gobierno y Policía del Municipio de Benito Juárez, Quintana Roo y 68 del Código Fiscal Municipal del  Estado de Quintana Roo. Asimismo, en el caso de no contar con la licencia de funcionamiento municipal al momento de la diligencia, se le apercibe al contribuyente que el establecimiento visitado podrá ser sujeto a clausura en cualquier momento, ello de conformidad con lo establecido en los artículos 479 y 509, fracciones IV y VII del Bando de Gobierno y Policía del Municipio de Benito Juárez, Quintana Roo, ya que para el ejercicio de cualquier actividad comercial, industrial o de servicios por parte de los contribuyentes, se requiere de permiso, licencia o autorización, según sea el caso, expedido por el Ayuntamiento, entendiéndose que no cuenta la autorización para funcionar si no exhibe la licencia de funcionamiento municipal correspondiente. Con lo anterior se da por concluida la presente diligencia, siendo las _____ horas con ______ minutos del día _______ del mes de _______________________ del año __________, se levanta el acta correspondiente, la cual se leyó en presencia de los que en ella intervinieron, haciéndole entrega al visitado de una copia de la presente con firmas autógrafas, la cual ratifican y firman para constancia los que en ella intervinieron y así quisieron hacerlo.</p>
+<br>
+<table class="aic-firma-inspector">
+	<tr>
+		<td>
+			<b class="mb-0">INSPECTOR, NOTIFICADOR-EJECUTOR</b>
+			<p class="mbt-0">________________________________________</p>
+			<p class="mbt-0"><b>NOMBRE Y FIRMA</b></p>
+		</td>
+	</tr>
+</table>
+<h4 class="text-center"><b>LOS TESTIGOS</b></h4>
+<table class="aic-firma-testigo mt-0">
+	<tr>
+		<td>
+			<p class="mbt-0">_________________________________</p>
+			<p><b>Nombre y Firma del Testigo de Asistencia</b></p>
+		</td>
+		<td>
+			<p class="mbt-0">_________________________________</p>
+			<p><b>Nombre y Firma del Testigo de Asistencia</b></p>
+		</td>
+	</tr>
+</table>
+<h4 class="text-center mt-0"><b>PERSONA CON LA QUE SE ENTENDIÓ LA VISITA DOMICILIARIA</b></h4>
+<p class="mbt-0 text-center mbt-0">_______________________________________________________________________</p>
+<p class="text-center"><b>Nombre y Firma</b></p>
+</div>
